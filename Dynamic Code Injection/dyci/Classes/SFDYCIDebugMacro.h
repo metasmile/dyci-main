105b63700a5872b8871975ceab3aae64f4b78518
metasmile <cyrano905@gmail.com>
2015-06-09 19:36:26 +0900

    Add some codes for checking block execution time.

--- Dynamic Code Injection/dyci/Classes/SFDYCIDebugMacro.h
+++ Dynamic Code Injection/dyci/Classes/SFDYCIDebugMacro.h
@@ -7,6 +7,7 @@
 //
 
 #import <objc/runtime.h>
+#import <mach/mach_time.h>
 
 /*
 A handy macro when working revert to original frequently + catch exceptions for protecting test cycle.
@@ -31,30 +32,50 @@ Then DYCI will call 'BEGIN_REJECTION ~ END_REJECTION' block. And repeat.
  */
 
 #define BEGIN_INJECTION - (void)updateOnClassInjection{; \
-    int __linebegin__ = __LINE__;\
-    BOOL __raised__ = NO;\
     NSLog(@"----------------- Injecting ... -----------------");\
-    @try{ \
+    double __exeinterval__ = DYCI_CHECK_DELAY(^{\
+        int __linebegin__ = __LINE__;\
+        BOOL __raised__ = NO;\
+        @try{\
 
 #define END_INJECTION \
-    }@catch(NSException *iex){\
-        NSLog(@"[!] Injecting error: %@",iex);\
-        __raised__ = YES;\
-    }\
-    SEL __selector_injection__ = NSSelectorFromString(@"__updateOnClassRejection");\
-    BOOL __exist_method__ = (BOOL) class_getInstanceMethod(self.class, __selector_injection__);\
-    if((__raised__ || __LINE__-__linebegin__==1) && __exist_method__){\
-        @try{ \
-            NSLog(@"----------------- Rejecting ... -----------------");\
-            ((void (*)(id, SEL))[self methodForSelector:__selector_injection__])(self, __selector_injection__);\
-        }@catch(NSException *rex){\
-             NSLog(@"[!] Rejecting error: %@",rex);\
-             __raised__ = YES;\
+        }@catch(NSException *iex){\
+            NSLog(@"[!] Injecting error: %@",iex);\
+            __raised__ = YES;\
         }\
-    }\
-    NSLog(@"-------------------------------------------------");\
+        SEL __selector_injection__ = NSSelectorFromString(@"__updateOnClassRejection");\
+        BOOL __exist_method__ = (BOOL) class_getInstanceMethod(self.class, __selector_injection__);\
+        if((__raised__ || __LINE__-__linebegin__==1) && __exist_method__){\
+            @try{ \
+                NSLog(@"----------------- Rejecting ... -----------------");\
+                ((void (*)(id, SEL))[self methodForSelector:__selector_injection__])(self, __selector_injection__);\
+            }@catch(NSException *rex){\
+                 NSLog(@"[!] Rejecting error: %@",rex);\
+                 __raised__ = YES;\
+            }\
+        }\
+    });\
+    NSLog(@"================= Complete [%lfs] =================",__exeinterval__);\
     }
 
 #define BEGIN_REJECTION - (void)__updateOnClassRejection{;\
 
 #define END_REJECTION }
+
+
+/*
+Utils
+ */
+static inline double
+DYCI_CHECK_DELAY(void (^block)(void))
+{
+    mach_timebase_info_data_t info;
+    if (mach_timebase_info(&info) != KERN_SUCCESS) return -1.0;
+
+    uint64_t start = mach_absolute_time ();
+    block();
+    uint64_t end = mach_absolute_time ();
+    uint64_t elapsed = end - start;
+    uint64_t nanos = elapsed * info.numer / info.denom;
+    return (double)nanos / NSEC_PER_SEC;
+}
